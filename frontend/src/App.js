9978ba4383f7106eb607c18bdf7b4eefb7f1ff6d
IgnjatMarko <ignjatovicmarko2023@gmail.com>
2024-03-14 19:36:56 +0100

    second commit
    Added BrowserRouter
    Created 5 pages (Cart, Product etc)
    Added Links to Navbar
    Created Hero page Component
    Created Item product Component
    Created Popular list Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory"
+import Product from "./Pages/Product"
+import Cart from "./Pages/Cart"
+import LoginSignup from "./Pages/LoginSignup";
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/shop2" element={<ShopCategory category="shop2" />} />
+          <Route path="/shop3" element={<ShopCategory category="shop3" />} />
+          <Route path="/shop4" element={<ShopCategory category="shop4" />} />
+          <Route path="/product" element={<Product/>}>
+            <Route path=":productId" element={<Product/>}/>
+          </Route>
+          <Route path="/cart" element={<Cart/>}/>
+          <Route path="/login" element={<LoginSignup/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,33 +1,71 @@
-import React, { useState } from 'react'
-import './Navbar.scss'
-import logo from '../Assets/logo.png'
-import cart_icon from '../Assets/cart_icon.png'
+import React, { useState } from "react";
+import "./Navbar.scss";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
-    const [menu, setMenu] = useState("shop");
+  const [menu, setMenu] = useState("shop");
 
   return (
-    <div className='navbar'>
-        <div className='nav-logo'>
-            <img src={logo} alt="logo" />
-            <p>Retro Video Club</p>
-        </div>
-        <ul className='nav-menu'>
-            <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr />:<></>}</li>
-            <li onClick={()=>{setMenu("shop2")}}>Shop 2{menu==="shop2"?<hr />:<></>}</li>
-            <li onClick={()=>{setMenu("shop3")}}>Shop 3{menu==="shop3"?<hr />:<></>}</li>
-            <li onClick={()=>{setMenu("shop4")}}>Shop 4{menu==="shop4"?<hr />:<></>}</li>
-        </ul>
-        <div className='nav-login-cart'>
-            <button>Login</button>
-            <img src={cart_icon} alt="cart" />
-            <div className="nav-cart-count">
-                0
-            </div>
-        </div>
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="logo" />
+        <p>Retro Video Club</p>
+      </div>
+      <ul className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/">
+            Shop
+          </Link>
+          {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("shop2");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/shop2">
+            Shop2
+          </Link>
+          {menu === "shop2" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("shop3");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/shop3">
+            Shop3
+          </Link>
+          {menu === "shop3" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("shop4");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/shop4">
+            Shop4
+          </Link>
+          {menu === "shop4" ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart_icon} alt="cart" />
+        </Link>
+        <div className="nav-cart-count">0</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
