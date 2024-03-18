2fcb1d01afa221e1056d02d255f5f6e40e6d9504
IgnjatMarko <ignjatovicmarko2023@gmail.com>
2024-03-18 23:35:24 +0100

    fourth commit
    Added Responsivity to many components (Navbar, Hero, Popular, Offers, Item, NewCollections, NewsLetter, Footer, ShopCategory)

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,31 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import "./Navbar.scss";
+
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
+import dropdown_icon from "../Assets/drop-down.png";
+
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../Context/ShopContext";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
   const {getTotalCartItems} = useContext(ShopContext);
+  const menuRef = useRef();
+
+  const dropdownToggle = (e) => {
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  }
 
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="logo" />
-        <p>Retro Video Club</p>
+        <p>Retro Video</p>
       </div>
-      <ul className="nav-menu">
+      <img className="nav-dropdown" onClick={dropdownToggle} src={dropdown_icon} alt="dropdown-icon" />
+      <ul ref={menuRef} className="nav-menu">
         <li
           onClick={() => {
             setMenu("shop");
